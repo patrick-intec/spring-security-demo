e45e9ae53ab508c635319974c469d1d3d170fa2e
Patrick <patrick.geudens@intecbrussel.be>
2021-12-12 18:27:00 +0100

    Spring Security Basic User authentication

--- src/main/java/be/infernalwhale/springsecurity/controller/HelloController.java
+++ src/main/java/be/infernalwhale/springsecurity/controller/HelloController.java
@@ -6,9 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @GetMapping("/login")
+    public ResponseEntity doLogin() {
+        return ResponseEntity.ok("Hello from the login controller");
+    }
 
     @GetMapping("/hello")
     public ResponseEntity hello() {
         return ResponseEntity.ok("Hello World, basic security");
+    }
+
+    @GetMapping("/restricted")
+    public ResponseEntity restrictedMethod() {
+        return ResponseEntity.ok("Hello World, restricted access");
     }
 }
